5da20e9e083d6ad616d8e68930033c21d90128e2
Dillon Allen <dillona@cos.edu>
2019-10-16 19:13:21 -0700

    Initial automation complete, next step will be to add voice activation

---
+++ Automation/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Automation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDo
+            // 
+            this.btnDo.Location = new System.Drawing.Point(153, 105);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(112, 43);
+            this.btnDo.TabIndex = 0;
+            this.btnDo.Text = "Do the thing";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.BtnDo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 266);
+            this.Controls.Add(this.btnDo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDo;
+    }
+}
+
---
+++ Automation/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Automation
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDo_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"C:\Users\Dillon's Laptop\AppData\Local\Programs\Microsoft VS Code\Code.exe");
+            
+            //Process.Start(@"C: \Users\Dillon's Laptop\AppData\Local\GitHubDesktop\GitHubDesktop.exe");
+            try
+            {
+                Process.Start("chrome.exe", "www.youtube.com");
+                Process.Start("chrome.exe", "www.github.com/dillonallen92/");
+                Process.Start("chrome.exe", "https://www.messenger.com/t/wyndom.wescott");
+
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.ToString() + "blahh");
+            }
+            
+
+        }
+    }
+}
